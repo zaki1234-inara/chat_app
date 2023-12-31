b7c4462e39eb99d1bb66b1ac5df380955735af99
Zakaria Zakaria <zakaria.inara1@gmail.com>
2023-12-30 21:18:36 -0500

    Update, so that anyone can access the chat app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import { ChatEngine, ChatFeed } from "react-chat-engine";
-
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
 const App = () => {
-return (
-    <ChatEngine 
-        height = "100vh"
-        projectID="7985c9ad-57bb-4d79-b1a6-245bd9f07249"
-        userName="harrypotter"
-        userSecret="0123"
-        renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} /> }
-    
-    />
-);
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
-}
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
